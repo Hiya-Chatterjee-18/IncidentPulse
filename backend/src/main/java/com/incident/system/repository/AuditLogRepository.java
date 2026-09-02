165a10f2a2afb0f80f482615aaf94304ef825b34
HIYA CHATTERJEE <hiya2005.chat@gmail.com>
2026-09-03 03:36:36 +0530

    Add AuditLogRepository interface for MongoDB

---
+++ backend/src/main/java/com/incident/system/repository/AuditLogRepository.java
@@ -0,0 +1,9 @@
+package com.incident.system.repository;
+
+import com.incident.system.model.AuditLog;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuditLogRepository extends MongoRepository<AuditLog, String> {
+}
